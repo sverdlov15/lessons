a884e59959849aa30d2928856cbf7cdb11817637
sverdlov15 <sverdlov15@gmail.com>
2022-05-25 00:17:23 +0300

    problem_solving_3.py

---
+++ lessons_03/problem_solving _3.py
@@ -0,0 +1,40 @@
+   ...: 
+   ...: print(olga + maxim + roman)
+   ...: 
+   ...: print("Exercise #4")
+   ...: print((olga + maxim + roman) / 3)
+   ...: 
+   ...: 
+   ...: print("Exercise #5")
+   ...: my_list = [1, 1.0, 2, 2, 5.0, "python", "python3", "python3"]
+   ...: my_set = set(my_list)
+   ...: print(len(my_list) - len(my_set))
+   ...: 
+   ...: 
+   ...: print("Exercise #6")
+   ...: my_slice = my_list[3:6]
+   ...: print(my_slice[::-1])
+   ...: 
+   ...: 
+   ...: print("Exercise #7")
+   ...: size = 13
+   ...: print([size * 4, size ** 2, size * size, (size ** 2 * 2) ** 0.5])
+   ...: 
+   ...: import math
+   ...: print([size * 4, size ** 2, size * size, math.sqrt(size ** 2 * 2)])
+Exercise #1
+1.5
+Exercise #2
+2.5
+Exercise #3
+98
+Exercise #4
+32.666666666666664
+Exercise #5
+3
+Exercise #6
+['python', 5.0, 2]
+Exercise #7
+[52, 169, 169, 18.384776310850235]
+[52, 169, 169, 18.384776310850235]
+
